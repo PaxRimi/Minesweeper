e3d56e76b47d3781925f1a9af86c6dfd4f37d51b
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-08-25 17:26:40 +0200

    Add script.js file with the starting DOMContentLoaded and class

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", function () {
+   class Minesweeper {
+       constructor(boardWidth, boardHeight){
+           this.width = boardWidth;
+           this.height = boardHeight;
+       }
+    }
+
+
+    
+
+});
